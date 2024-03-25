c17beb39758d156828d3c58d5c7dbc41d9886fdf
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-2.local>
2024-03-24 21:39:28 -0400

    update

---
+++ data_generator.py
@@ -0,0 +1,40 @@
+import random
+out_verb=['诬陷','拥抱','靠近', '离开', '打扰', '超过', '忘了','回答',
+          '表扬','反对','调查','拜访','偷听','暗恋','邀请','等待','嫉妒',
+          '羡慕','搭讪','欢迎','监视','吵醒','激怒','模仿','欺负','追随',
+          '回避','思念','仰慕','冷落','吸引']
+control_verb = ['让']
+speech_verb = ['知道', '希望', '坚信','声称','以为','怀疑','觉得','预料','推断','暗示','表示']
+in_verb = ['反思', '反省','坦白', '提升', '表达','调节','']
+amb_verbs = ['喜欢','讨厌', '厌恶','帮助','相信','支持','欺骗','安慰',
+            '挑战','鄙视','忽略','原谅','怀疑','超越','表扬',
+            '鞭策', '尊重','辜负','担心','控制']
+inanimate_nouns =['树','窗户','展出',]
+female ='她'
+male ='他'
+first_person = '我'
+second_person = '你'
+
+
+# ambiguous
+def create_ambiguous_sentences(amb_verbs, output_file,female_first=True):
+    with open(output_file, 'w') as out:
+        for amb_verb in amb_verbs:
+            matrix_verb = random.choice(speech_verb)
+            if female_first:
+                out_sent = female+matrix_verb+male+amb_verb+'自己。'
+            else:
+                out_sent = male + matrix_verb + female + amb_verb + '自己。'
+
+            out.write(f'{out_sent}\n')
+
+# blocking effect
+# def create_blocking_effect_pairs():
+
+# subject/speaker orientation
+
+# animacy effect
+
+if __name__=='__main__':
+    create_ambiguous_sentences(amb_verbs, output_file='data/amb_f1.txt')
+    create_ambiguous_sentences(amb_verbs, output_file='data/amb_m1.txt', female_first=False)
---
+++ nmt.py
@@ -0,0 +1,29 @@
+from transformers import MarianTokenizer, MarianMTModel
+from pahtlib import Path
+
+tokenizer = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
+model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
+
+amb_f1 = Path('data/amb_f1.txt').read_text().strip().split('\n')
+amb_m1 = Path('data/amb_m1.txt').read_text().strip().split('\n')
+
+
+def get_prediction(zh_sents, female_first=True):
+    srcs = zh_sents
+    batch = tokenizer.prepare_seq2seq_batch(src_texts=srcs, return_tensors="pt")
+    if female_first:
+        outputs_beam = model.generate(
+            **batch, num_beams=5, max_length=512,
+            return_dict_in_generate=True, output_hidden_states=True,
+            output_attentions=True)
+
+        detokenised_prds = tokenizer.batch_decode(
+            outputs_beam["sequences"], skip_special_tokens=True)
+
+        for j, src in enumerate(srcs):
+            print(src)
+            print(detokenised_prds[j])
+
+
+
+
